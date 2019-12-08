9b2dbb894f410835f8e3990f49da314f55469731
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 10:50:23 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+def map(array_of_values)
+  newArray = []
+  i = 0
+  while i < array_of_values.length do
+    newArray.push(yield(array_of_values[i]))
+    i += 1
+  end
+  newArray
+end
+
+def reduce(array_of_values, startingPoint = 0)
+  myTotes = startingPoint
+  i = 0
+  while i < array_of_values.length do
+    myTotes = yield(myTotes, array_of_values[i])
+    i += 1
+  end
+  
+  if myTotes == 0
+    myTotes = true
+  end
+  myTotes
+end
